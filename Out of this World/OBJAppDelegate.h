ef32f88ba45da955f3746428a431b297c71880d7
Jimi Ogunyomi <jimmysyl@Jimis-MacBook-Pro.local>
2014-04-07 21:12:22 +0100

    Initial Commit

---
+++ Out of this World/OBJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OBJAppDelegate.h
+//  Out of this World
+//
+//  Created by Jimi Ogunyomi on 07/04/2014.
+//  Copyright (c) 2014 self.crystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/OBJViewController.h
@@ -0,0 +1,13 @@
+//
+//  OBJViewController.h
+//  Out of this World
+//
+//  Created by Jimi Ogunyomi on 07/04/2014.
+//  Copyright (c) 2014 self.crystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBJViewController : UIViewController
+
+@end
